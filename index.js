e600348235f3fb2dd1de94fa5982116de79a6cb3
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-07-25 12:37:36 +0530

    rendered static files on the browser using path.join(__dirname,'public')

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
-app.get('/',(req,res)=>{
-    res.send('hello, this is home page')
+const folderPath = path.join(__dirname,'/public')
+console.log(folderPath)
+
+app.use(express.static(folderPath))
+// app.get('/',(req,res)=>{
+//     res.send('hello, this is home page')
 
 
-})
+// })
 
-app.get('/about',(req,res)=>{
-    res.send('we are the best ')
-})
+// app.get('/about',(req,res)=>{
+//     res.send('we are the best ')
+// })
 
 app.listen(3000)
