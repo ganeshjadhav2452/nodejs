c8426e50d14bfa115349175f2cee8199b98afb3d
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-07-25 15:39:35 +0530

    learned about template engine 'ejs'

--- index.js
+++ index.js
@@ -3,9 +3,41 @@ const app = express()
 const path = require('path')
 
 const folderPath = path.join(__dirname,'/public')
-console.log(folderPath)
 
-app.use(express.static(folderPath))
+app.set('view engine', 'ejs')
+app.get('/',(request, response)=>{
+    const user={
+        name:'ganesh jadhav',
+        email:'ganeshjadhav2452@gmail.com',
+        city:'chatrpati sambhaji nagar',
+        skills:['js','php','java']
+
+    }
+    response.render(`profile`,{user})
+})
+
+app.get('/about',(request, response)=>{
+    response.sendFile(`${folderPath}/about.html`)
+})
+app.get('/login',(request, response)=>{
+    response.render('login')
+})
+
+
+app.get('/help',(request, response)=>{
+    response.sendFile(`${folderPath}/help.html`)
+})
+
+app.get('*',(request, response)=>{
+    response.sendFile(`${folderPath}/notFound.html`)
+})
+
+
+
+// console.log(folderPath)
+
+// app.use(express.static(folderPath))
+
 // app.get('/',(req,res)=>{
 //     res.send('hello, this is home page')
 
@@ -16,4 +48,4 @@ app.use(express.static(folderPath))
 //     res.send('we are the best ')
 // })
 
-app.listen(3000)
+app.listen(4000)
