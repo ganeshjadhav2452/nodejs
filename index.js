307d6fcc59d04f61602cedef9905f5e953713f19
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-07-26 12:30:39 +0530

    connected mongodb to nodejs

--- index.js
+++ index.js
@@ -1,47 +1,21 @@
 const express = require('express')
 const app = express()
-const path = require('path')
-const ageFilter = require('./middleware')
 
+const {MongoClient} = require('mongodb')
+const dataBase = 'ganesh-jadhav'
+const url = 'mongodb://0.0.0.0:27017'
 
-const route = express.Router()
+const client = new MongoClient(url);
 
-route.use(ageFilter)
-// app.use(ageFilter) // --> if we want to  use middleware on spesific rout then  instead of passing this function to app.use() we can pass the middleware function as sencond paramter in the app.get (means after the url)
+async function getData(){
+    let result = await client.connect();
+   const myDataBase =  result.db(dataBase)
+   const collection =  myDataBase.collection('products')
+    const data = await collection.find({}).toArray()
+   console.log(data)
 
-app.set('view engine', 'ejs')
-app.get('/', (request, response)=>{
-    const user={
-        name:'ganesh jadhav',
-        email:'ganeshjadhav2452@gmail.com',
-        city:'chatrpati sambhaji nagar',
-        skills:['js','php','java']
+}
 
-    }
-    response.render(`profile`,{user})
-})
+getData()
 
-route.get('/login',(request, response)=>{
-    response.render('login')
-})
-app.use('/', route)
-
-
-
-
-
-// console.log(folderPath)
-
-// app.use(express.static(folderPath))
-
-// app.get('/',(req,res)=>{
-//     res.send('hello, this is home page')
-
-
-// })
-
-// app.get('/about',(req,res)=>{
-//     res.send('we are the best ')
-// })
-
-app.listen(4000)
+app.listen(3000)
