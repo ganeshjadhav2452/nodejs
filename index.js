1b9f67bab045fda9721f86c3e31dbd472265b137
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-07-26 17:10:45 +0530

    posted data on mongo db server using user request

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const connectDB = require('./mongoDB')
 const express = require('express')
+
 const app = express();
-
-
+ app.use(express.json())
+// sending data of mongoDB of on browser
 app.get('/',async(request,response)=>{
     let connection = await connectDB();
     const data = await connection.find({}).toArray()
@@ -13,6 +14,13 @@ app.get('/',async(request,response)=>{
 
 })
 
+app.post('/',async(request,response)=>{
+    let data =await connectDB();
+    data = await data.insertOne(request.body)
+    response.send(request.body)
+
+})
 
 
-app.listen(3000)
+
+app.listen(4000)
