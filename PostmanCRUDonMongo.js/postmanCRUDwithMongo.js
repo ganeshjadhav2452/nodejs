f6a2d3669fdfec6119c9025655571baed9fab620
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-07-27 12:47:06 +0530

    postman crud with mongodb

---
+++ PostmanCRUDonMongo.js/postmanCRUDwithMongo.js
@@ -0,0 +1,44 @@
+const connectDB = require('../mongoDB')
+const express = require('express')
+
+const app = express();
+ app.use(express.json())
+// sending data of mongoDB of on browser
+app.get('/',async(request,response)=>{
+    let connection = await connectDB();
+    const data = await connection.find({}).toArray()
+    console.log(data)
+
+    response.send(data)
+
+
+})
+
+// post data on server
+app.post('/',async(request,response)=>{
+    let data =await connectDB();
+    data = await data.insertOne(request.body)
+    response.send(request.body)
+
+})
+
+app.put('/',async(request,response)=>{
+    // let data = await connectDB()
+    // // data = await data.updateOne(request.body);
+    // response.send(request.body)
+
+    // not understood
+   
+})
+
+app.delete('/',async(request, response)=>{
+    
+    let data = await connectDB();
+        data = await data.deleteMany(request.body)
+    console.log(request.body)
+    response.send(' deleted')
+})
+
+
+
+app.listen(4000)
