db77cd64ad1439dde9611c241313fc2001009d1d
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-07-27 12:24:00 +0530

    delete data using url request

--- index.js
+++ index.js
@@ -14,11 +14,29 @@ app.get('/',async(request,response)=>{
 
 })
 
+// post data on server
 app.post('/',async(request,response)=>{
     let data =await connectDB();
     data = await data.insertOne(request.body)
     response.send(request.body)
 
+})
+
+app.put('/',async(request,response)=>{
+    // let data = await connectDB()
+    // // data = await data.updateOne(request.body);
+    // response.send(request.body)
+
+    // not understood
+   
+})
+
+app.delete('/',async(request, response)=>{
+    
+    let data = await connectDB();
+        data = await data.deleteMany(request.body)
+    console.log(request.body)
+    response.send(' deleted')
 })
 
 
